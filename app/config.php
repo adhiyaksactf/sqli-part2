53fc03b71fce74ae88b63aed553818c059f75438
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2023-03-12 15:09:17 +0700

    docker

--- app/config.php
+++ app/config.php
@@ -2,6 +2,5 @@
     error_reporting(E_ALL);
     ini_set('display_errors', TRUE);
     ini_set('display_startup_errors', TRUE);
-    $conn = mysqli_connect("localhost","root","");
+    $conn = mysqli_connect("db","root","root");
     mysqli_select_db($conn, 'sqli-lab');
-?>

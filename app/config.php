a607d8636a9b7eaa65dace2944e9780f337a6dfa
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2023-03-12 19:22:03 +0700

    change db name in config & add depends_on lin in compose

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', TRUE);
-    ini_set('display_startup_errors', TRUE);
-    $conn = mysqli_connect("db","root","root");
-    mysqli_select_db($conn, 'sqli-lab');
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+$conn = mysqli_connect("db", "sqli", "sqli");
+mysqli_select_db($conn, 'sqli');
